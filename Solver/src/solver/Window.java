7f602e55b8931828ebffc95e7cd9ecad8fa97a59
skogstall <gillis.oldfeldt@gmail.com>
2018-02-21 13:47:58 +0100

    test

---
+++ Solver/src/solver/Window.java
@@ -0,0 +1,20 @@
+package solver;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Window extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
