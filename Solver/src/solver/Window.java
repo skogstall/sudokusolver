6079bae5924e8387f5ef7d4652c98d7c63961cbc
Hackspett <fridajohnsson135@gmail.com>
2018-02-22 09:15:29 +0100

    Test frida

--- Solver/src/solver/Window.java
+++ Solver/src/solver/Window.java
@@ -5,7 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class Window extends Application{
@@ -15,6 +19,33 @@ public class Window extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        BorderPane groot = new BorderPane();
+        TilePane tilePane = new TilePane();
+        tilePane.setPrefRows(9);
+        tilePane.setPrefColumns(9);
+        tilePane.setPrefWidth();
+        tilePane.setPrefHeight();
+
+        HBox knappar = new HBox();
+
+        Scene scene = new Scene(groot, 500, 500);
+        primaryStage.setTitle("Skogstall och Hackspetts Sudoku");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        for(int i = 1; i<81; i++){
+            TextField t = new TextField();
+            t.setPrefSize(40,40);
+            tilePane.getChildren().add(t);
+        }
+        for(int i = 0; i< tilePane.getChildren().size() - 1; i++){
+        }
+
+
+
+
+        groot.setTop(tilePane);
 
     }
 }
