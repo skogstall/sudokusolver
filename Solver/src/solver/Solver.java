13a79f76c819f4848b34b050ebbdda2779af37f3
skogstall <gillis.oldfeldt@gmail.com>
2018-03-05 13:04:14 +0100

    uh

--- Solver/src/solver/Solver.java
+++ Solver/src/solver/Solver.java
@@ -46,7 +46,10 @@ public class Solver {
         int index = 0;
         for(int i = 0; i <= 8; i++){
             for(int j = 0; j <= 8; j++){
-                arr[index] = sudoku[i][j] ;
+                if (sudoku[i][j] == null)
+                {arr[index] = 0;}
+                else {arr[index] = sudoku[i][j];}
+
                 index++;
             }
         }
